759c6aa3c5ae17efeb59f2c18a3d0ce710b4e1d7
jyoing2 <2019112966@dongguk.edu>
2023-07-20 23:00:24 +0900

    plus

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Header() {
   return <h1>동국대학교 멋쟁이 사자처럼 뉴우스</h1>;
@@ -65,8 +65,56 @@ function FakeNews() {
       <Section4 />
       <hr />
       <Button onClick={handleButtonClick} />
+      <hr />
+      <hr />
+      <App />
     </>
   );
 }
+
+function App() {
+  const [time, setTime] = useState(RemainTime());
+  const [prevmin, setPrevmin] = useState(time.min);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const newTime = RemainTime();
+      setTime(newTime);
+
+      if (newTime.min !== prevmin) {
+        const minPassed = newTime.min - prevmin;
+        console.log(`그새 1분이 더 지났어..`);
+        setPrevmin(newTime.min);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [prevmin]);
+
+  function RemainTime() {
+    const now = new Date();
+    const day = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    const timer = day - now;
+
+    const hours = Math.floor(timer / (1000 * 60 * 60));
+    const min = Math.floor((timer % (1000 * 60 * 60)) / (1000 * 60));
+    const sec = Math.floor((timer % (1000 * 60)) / 1000);
+
+    return {
+      hours: hours,
+      min: min,
+      sec: sec,
+    };
+  }
+
+  return (
+    <div>
+        벌써 하루가 {time.hours}:{time.min}:{time.sec} 밖에 안남았어..!
+    </div>
+  );
+}
+
 
 export default FakeNews;
